6a64e9db6691372bf0930d55b0f2e3a49c35842f
Chris Lim <cli333@protonmail.com>
2020-04-28 13:45:14 -0700

    updated travis.yml

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,31 +2,31 @@ import React from "react";
 import { render } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link1", () => {
+test("renders hello docker", () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/Hello DOCKER@@!/i);
   expect(linkElement).toBeInTheDocument();
 });
 
-test("renders learn react link2", () => {
+test("renders hello docker", () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/Hello DOCKER@@!/i);
   expect(linkElement).toBeInTheDocument();
 });
 
-test("renders learn react link2", () => {
+test("renders hello docker", () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/Hello DOCKER@@!/i);
   expect(linkElement).toBeInTheDocument();
 });
 
-test("renders learn react link2", () => {
+test("renders hello docker", () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/Hello DOCKER@@!/i);
   expect(linkElement).toBeInTheDocument();
 });
 
-test("renders learn react link2", () => {
+test("renders hello docker", () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/Hello DOCKER@@!/i);
   expect(linkElement).toBeInTheDocument();
